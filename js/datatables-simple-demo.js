0f29cb10d6bf12427a5e7b146c051852202d46f9
Lavish-703 <82791010+Lavish-703@users.noreply.github.com>
2022-09-13 17:22:01 +0530

    Add files via upload

---
+++ js/datatables-simple-demo.js
@@ -0,0 +1,9 @@
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   const datatablesSimple = document.getElementById("datatablesSimple");
+//   if (datatablesSimple) {
+//     new simpleDatatables.DataTable(datatablesSimple);
+//   }
+// });
+
+// Simple-DataTables
+// https://github.com/fiduswriter/Simple-DataTables/wiki
